388cf3a104cc1a5003b0bc4b030076e51b90e03b
jcarr <j5carroll@student.bridgew.edu>
2021-09-23 10:12:06 -0400

    New bank account

---
+++ src/Customer.java
@@ -0,0 +1,27 @@
+import com.company.BankAccount;
+
+import java.util.ArrayList;
+
+public class Customer {
+    private int customerID;
+    private String name;
+    private ArrayList<BankAccount> accounts;
+
+    public Customer(String CustomerName,int taxID){
+        customerID = taxID;
+        name = CustomerName;
+
+    }
+    public boolean openAccount(double initialDeposit){
+        var newAccount = new BankAccount();
+        newAccount.deposit(initialDeposit);
+        var didSucceed = accounts.add(newAccount);
+        return didSucceed;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getCustomerID(){
+        return customerID;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	var myAccount = new BankAccount();
-    var yourAccount = new BankAccount(intialBlance 2000, initialRate = 0.05f);
+    var yourAccount = new BankAccount(2000,0.05f);
     myAccount.deposit(1000);
     var newBalance = myAccount.addIntrest();
     var yourBlance =yourAccount.addIntrest();
